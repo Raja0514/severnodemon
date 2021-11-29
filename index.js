0e55cb55a04bb72f03821a5411a9a2008d07313a
Raja0514 <rajamr1987@gmail.com>
2021-11-29 21:37:38 +0400

    commit2

--- index.js
+++ index.js
@@ -1,17 +1,66 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+app.use(express.json());
+app.use(cors());
+const port = process.env.PORT || "3002";
 
-const express =require('express')
+app.get("/", (req, res) => {
+  res.send("server running sucessfully......");
+});
+//mongodb connection
+mongoose.connect(
+  "mongodb+srv://admin:rajamoni051412@test.q8lbl.mongodb.net/NewDatabase?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("DB connected");
+  }
+);
+const userschema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+});
+const User = new mongoose.model("usercollections", userschema);
+//routes
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
 
-const app=express();
+  User.findOne({ email: email }, (err, user) => {
+    if (user) {
+      if (password === user.password) {
+        res.send({ message: "login succesfull", user: user });
+      } else {
+        res.send({ message: "password did not match" });
+      }
+    } else {
+      res.send({ message: "user not registred" });
+    }
+  });
+});
+app.post("/register", (req, res) => {
+  const { name, email, password, reEnterpassword } = req.body;
 
-const port=process.env.PORT || '3002';
+  User.findOne({ email: email }, (err, user) => {
+    if (user) {
+      res.send({ message: "user already registred" });
+    } else {
+      const user = new User({ name, email, password });
 
-app.get("/",(req,res)=>{
-
-    res.send("server running sucessfully with nodemon......")
-})
-app.listen(port,()=>{
-
-    console.log(`server running at port no ${port} `)
-})
-
-
+      user.save((err) => {
+        if (err) {
+          res.send(err);
+        } else {
+          res.send({ message: "successfully registered please login now" });
+        }
+      });
+    }
+  });
+});
+app.listen(port, () => {
+  console.log(`server running at port no ${port} `);
+});
